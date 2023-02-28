e7ee96287fcf69a00273b01e26e89ab2757b0902
OmraniWassim <omraniwassim1@essths.u-sousse.tn>
2023-02-28 12:12:42 +0100

    commit

--- src/main/java/com/ltms/pfe/PfeApplication.java
+++ src/main/java/com/ltms/pfe/PfeApplication.java
@@ -8,7 +8,7 @@ public class PfeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PfeApplication.class, args);
-		System.out.println("git working");
+		System.out.println("git working fine");
 
 	}
 
